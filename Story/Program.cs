d2da08e6e634161ae69c1ce4fcbec17ac8894e04
Noui Ljung Boulala <noui.ljung.boulala@gmail.com>
2026-09-17 15:57:21 +0200

    jhbly

---
+++ Story/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hej");
+Console.WriteLine("Du har två val du gå med hugo in i en mörk grotta eller så går du hem");
+string svar 2 = Console ReadLine();
+if (svar1== "går hem"
+string svar1 = Console.ReadLine();
+if (svar1 == "hugo")
+{
+    Console.WriteLine("Så du går ned i grottan och du ser är svärd tar du det och mördar hugo eller går vidare");
+}
+Console.ReadLine();
